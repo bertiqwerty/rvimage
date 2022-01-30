66626729aba17bda620c08c5f997d8821efa3ce5
Behrang Shafei <behrang.shafei@basf.com>
2022-01-30 20:06:45 +0100

    trying out pixels with egui

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,169 @@
-use iced::{executor, image, Application, Clipboard, Command, Element, Row, Settings, Text};
+#![deny(clippy::all)]
+#![forbid(unsafe_code)]
 
-struct RimView {
-    image: image::Handle,
-    image_viewer: image::viewer::State,
+use crate::gui::Framework;
+use image::{ImageBuffer, Rgb};
+use log::error;
+use pixels::{Error, Pixels, SurfaceTexture};
+use winit::dpi::LogicalSize;
+use winit::event::{Event, VirtualKeyCode};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+use winit_input_helper::WinitInputHelper;
+
+mod gui;
+
+const WIDTH: u32 = 256;
+const HEIGHT: u32 = 256;
+const BOX_SIZE: i16 = 64;
+
+/// Representation of the application state. In this example, a box will bounce around the screen.
+struct World {
+    box_x: i16,
+    box_y: i16,
+    velocity_x: i16,
+    velocity_y: i16,
+    image: ImageBuffer<Rgb<u8>, Vec<u8>>,
 }
 
-impl Application for RimView {
-    type Executor = executor::Default;
-    type Message = ();
-    type Flags = ();
-    fn new(_flags: ()) -> (RimView, Command<Self::Message>) {
-        (
-            RimView {
-                image: image::Handle::from_path(
-                    "C:/Users/shafeib/Desktop/2_1_1_2022-01-10_14-31-13.png",
-                ),
-                image_viewer: image::viewer::State::new(),
-            },
-            Command::none(),
-        )
-    }
+fn main() -> Result<(), Error> {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let mut input = WinitInputHelper::new();
+    let window = {
+        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+        WindowBuilder::new()
+            .with_title("Hello Pixels + egui")
+            .with_inner_size(size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
+    };
 
-    fn title(&self) -> String {
-        String::from("A cool application")
-    }
+    let (mut pixels, mut framework) = {
+        let window_size = window.inner_size();
+        let scale_factor = window.scale_factor() as f32;
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        let pixels = Pixels::new(WIDTH, HEIGHT, surface_texture)?;
+        let framework =
+            Framework::new(window_size.width, window_size.height, scale_factor, &pixels);
 
-    fn update(
-        &mut self,
-        _message: Self::Message,
-        _clipboard: &mut Clipboard,
-    ) -> Command<Self::Message> {
-        Command::none()
-    }
+        (pixels, framework)
+    };
 
-    fn view(&mut self) -> Element<Self::Message> {
-        Row::new()
-            .push(image::Viewer::new(
-                &mut self.image_viewer,
-                self.image.clone(),
-            ))
-            .into()
-    }
+    let image = image::io::Reader::open("c:/Users/ShafeiB/Desktop/paprika.png").unwrap().decode().unwrap();
+    let image = image.into_rgb8();
+    let mut world = World::new(image);
+
+    event_loop.run(move |event, _, control_flow| {
+        // Handle input events
+        if input.update(&event) {
+            // Close events
+            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+
+            // Update the scale factor
+            if let Some(scale_factor) = input.scale_factor() {
+                framework.scale_factor(scale_factor);
+            }
+
+            // Resize the window
+            if let Some(size) = input.window_resized() {
+                pixels.resize_surface(size.width, size.height);
+                framework.resize(size.width, size.height);
+            }
+
+            // Update internal state and request a redraw
+            world.update();
+            window.request_redraw();
+        }
+
+        match event {
+            Event::WindowEvent { event, .. } => {
+                // Update egui inputs
+                framework.handle_event(&event);
+            }
+            // Draw the current frame
+            Event::RedrawRequested(_) => {
+                // Draw the world
+                world.draw(pixels.get_frame());
+
+                // Prepare egui
+                framework.prepare(&window);
+
+                // Render everything together
+                let render_result = pixels.render_with(|encoder, render_target, context| {
+                    // Render the world texture
+                    context.scaling_renderer.render(encoder, render_target);
+
+                    // Render egui
+                    framework.render(encoder, render_target, context)?;
+
+                    Ok(())
+                });
+
+                // Basic error handling
+                if render_result
+                    .map_err(|e| error!("pixels.render() failed: {}", e))
+                    .is_err()
+                {
+                    *control_flow = ControlFlow::Exit;
+                }
+            }
+            _ => (),
+        }
+    });
 }
 
-fn main() -> iced::Result {
-    RimView::run(Settings::default())
+impl World {
+    /// Create a new `World` instance that can draw a moving box.
+    fn new(image: ImageBuffer<Rgb<u8>, Vec::<u8>>) -> Self {
+        Self {
+            box_x: 24,
+            box_y: 16,
+            velocity_x: 1,
+            velocity_y: 1,
+            image,
+        }
+    }
+
+    /// Update the `World` internal state; bounce the box around the screen.
+    fn update(&mut self) {
+        if self.box_x <= 0 || self.box_x + BOX_SIZE > WIDTH as i16 {
+            self.velocity_x *= -1;
+        }
+        if self.box_y <= 0 || self.box_y + BOX_SIZE > HEIGHT as i16 {
+            self.velocity_y *= -1;
+        }
+
+        self.box_x += self.velocity_x;
+        self.box_y += self.velocity_y;
+    }
+
+    /// Draw the `World` state to the frame buffer.
+    ///
+    /// Assumes the default texture format: `wgpu::TextureFormat::Rgba8UnormSrgb`
+    fn draw(&self, frame: &mut [u8]) {
+        
+        for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
+            let x = (i % WIDTH as usize) as i16;
+            let y = (i / WIDTH as usize) as i16;
+            let rgb = self.image.get_pixel(x as u32, y as u32).0;
+            let rgba = [rgb[0], rgb[1], rgb[2], 0xff];
+            // let inside_the_box = x >= self.box_x
+            //     && x < self.box_x + BOX_SIZE
+            //     && y >= self.box_y
+            //     && y < self.box_y + BOX_SIZE;
+
+            // let rgba = if inside_the_box {
+            //     [0x5e, 0x48, 0xe8, 0xff]
+            // } else {
+            //     [0x48, 0xb2, 0xe8, 0xff]
+            // };
+            
+            pixel.copy_from_slice(&rgba);
+        }
+    }
 }
