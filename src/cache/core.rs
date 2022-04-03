b41f79dfff51fb7bf9c0569dfd9e36c5a6387ebb
Behrang Shafei <behrang.shafei@basf.com>
2022-04-03 17:48:33 +0200

    part of previous commit

---
+++ src/cache/core.rs
@@ -0,0 +1,17 @@
+use image::{ImageBuffer, Rgb};
+
+use crate::result::RvResult;
+
+
+pub type ResultImage = RvResult<ImageBuffer<Rgb<u8>, Vec<u8>>>;
+
+pub trait ImageReaderFn {
+    fn read(local_path: &str) -> ResultImage;
+}
+
+pub trait Preload {
+    fn read_image(&mut self, selected_file_idx: usize, files: &[String]) -> ResultImage;
+    fn new() -> Self;
+}
+
+
