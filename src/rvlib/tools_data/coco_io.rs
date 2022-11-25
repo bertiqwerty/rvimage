ec15c5c8114aa45a49fbcf0c0c3d4c502a8410cf
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2022-11-25 10:16:36 +0100

    coco supports absolute and with relative coordinates

--- src/rvlib/tools_data/coco_io.rs
+++ src/rvlib/tools_data/coco_io.rs
@@ -42,10 +42,7 @@ struct CocoAnnotation {
     id: u32,
     image_id: u32,
     category_id: u32,
-    segmentation: Vec<Vec<u32>>,
-    area: f32,
-    bbox: [u32; 4],
-    iscrowd: u8,
+    bbox: [f32; 4],
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -109,10 +106,7 @@ impl CocoExportData {
                         id: box_id - 1,
                         image_id: image_idx as u32,
                         category_id: export_data.cat_ids[*cat_idx] as u32,
-                        bbox: [bb.x, bb.y, bb.w, bb.h],
-                        segmentation: vec![],
-                        area: (bb.h * bb.w) as f32,
-                        iscrowd: 0,
+                        bbox: [bb.x as f32, bb.y as f32, bb.w as f32, bb.h as f32],
                     }
                 })
                 .collect::<Vec<_>>()
@@ -146,12 +140,35 @@ impl CocoExportData {
         let id_image_map = self
             .images
             .iter()
-            .map(|coco_image: &CocoImage| Ok((coco_image.id, coco_image.file_name.as_str())))
-            .collect::<RvResult<HashMap<u32, &str>>>()?;
+            .map(|coco_image: &CocoImage| {
+                Ok((
+                    coco_image.id,
+                    (
+                        coco_image.file_name.as_str(),
+                        coco_image.width,
+                        coco_image.height,
+                    ),
+                ))
+            })
+            .collect::<RvResult<HashMap<u32, (&str, u32, u32)>>>()?;
 
         let mut annotations: HashMap<String, (Vec<BB>, Vec<usize>)> = HashMap::new();
         for coco_anno in self.annotations {
-            let bb = BB::from_array(&coco_anno.bbox);
+            let (file_name, w, h) = id_image_map[&coco_anno.image_id];
+
+            let (w_factor, h_factor) = if coco_anno.bbox.iter().all(|x| *x <= 1.0) {
+                (w as f32, h as f32)
+            } else {
+                (1.0, 1.0)
+            };
+            let bbox = [
+                (w_factor * coco_anno.bbox[0]).round() as u32,
+                (h_factor * coco_anno.bbox[1]).round() as u32,
+                (w_factor * coco_anno.bbox[2]).round() as u32,
+                (h_factor * coco_anno.bbox[3]).round() as u32,
+            ];
+
+            let bb = BB::from_array(&bbox);
             let cat_idx = cat_ids
                 .iter()
                 .position(|cat_id| *cat_id == coco_anno.category_id)
@@ -162,7 +179,7 @@ impl CocoExportData {
                         cat_ids
                     )
                 })?;
-            let k: &str = id_image_map[&coco_anno.image_id];
+            let k: &str = file_name;
             if let Some(annos_of_image) = annotations.get_mut(k) {
                 annos_of_image.0.push(bb);
                 annos_of_image.1.push(cat_idx);
@@ -239,13 +256,40 @@ fn test_coco_export() -> RvResult<()> {
 
 #[test]
 fn test_coco_import() -> RvResult<()> {
-    fn test(filename: &str, cat_ids: Vec<u32>) -> RvResult<()> {
+    fn test(filename: &str, cat_ids: Vec<u32>, bbs: &[(BB, &str)]) -> RvResult<()> {
         let read = read_coco(filename)?;
         assert_eq!(read.cat_ids(), &cat_ids);
         assert_eq!(read.labels(), &vec!["first label", "second label"]);
+        for (bb, file_path) in bbs {
+            let annos = read.get_annos(file_path);
+            assert!(annos.bbs().contains(bb));
+        }
         Ok(())
     }
-    test("resources/test_data/coco_catids_12.json", vec![1, 2])?;
-    test("resources/test_data/coco_catids_12.json", vec![1, 2])?;
+    let bb_im_ref_abs = [
+        (BB::from_array(&[1, 1, 5, 5]), "nowhere.png"),
+        (BB::from_array(&[11, 11, 4, 7]), "nowhere.png"),
+        (BB::from_array(&[1, 1, 5, 5]), "nowhere2.png"),
+    ];
+    let bb_im_ref_relative = [
+        (BB::from_array(&[10, 100, 50, 500]), "nowhere.png"),
+        (BB::from_array(&[91, 870, 15, 150]), "nowhere.png"),
+        (BB::from_array(&[10, 1, 50, 5]), "nowhere2.png"),
+    ];
+    test(
+        "resources/test_data/coco_catids_12.json",
+        vec![1, 2],
+        &bb_im_ref_abs,
+    )?;
+    test(
+        "resources/test_data/coco_catids_01.json",
+        vec![0, 1],
+        &bb_im_ref_abs,
+    )?;
+    test(
+        "resources/test_data/coco_catids_12_relative.json",
+        vec![1, 2],
+        &bb_im_ref_relative,
+    )?;
     Ok(())
 }
