faffdf40d27f3eb9f799155e87d516297081458a
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2022-11-26 15:41:03 +0100

    file missed at of previous commit, #1

---
+++ src/rvlib/reader/py_http_reader.rs
@@ -0,0 +1,54 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+
+use crate::{
+    cache::ReadImageToCache,
+    result::{to_rv, RvResult},
+    types::ResultImage,
+};
+
+#[derive(Clone)]
+pub struct ReadImageFromPyHttp;
+
+impl ReadImageToCache<()> for ReadImageFromPyHttp {
+    fn new(_: ()) -> RvResult<Self> {
+        Ok(Self {})
+    }
+    fn read(&self, url: &str) -> ResultImage {
+        let resp = || reqwest::blocking::get(url)?.bytes();
+        let image_byte_blob = resp().map_err(to_rv)?;
+        image::load_from_memory(&image_byte_blob).map_err(to_rv)
+    }
+    fn ls(&self, server_address: &str) -> RvResult<Vec<String>> {
+        lazy_static! {
+            static ref LI_REGEX: Regex = Regex::new(r"<li>.*</li>").unwrap();
+        }
+        lazy_static! {
+            static ref HREF_REGEX: Regex = Regex::new("href\\s*=\\s*\".*\"").unwrap();
+        }
+        let resp = || reqwest::blocking::get(server_address)?.text();
+        let text = resp().map_err(to_rv)?;
+        Ok(LI_REGEX
+            .find_iter(&text)
+            .flat_map(|found| {
+                let li_text = &text[found.start()..found.end()];
+                let found_href = HREF_REGEX.find(li_text);
+                let len_href = "href=\"".len();
+                found_href.map(|fh| {
+                    format!(
+                        "{}/{}",
+                        server_address,
+                        &li_text[(fh.start() + len_href)..(fh.end() - 1)]
+                    )
+                })
+            })
+            .collect())
+    }
+}
+
+#[test]
+fn test_read_image() -> RvResult<()> {
+    let x = ReadImageFromPyHttp {};
+    x.ls("")?;
+    Ok(())
+}
