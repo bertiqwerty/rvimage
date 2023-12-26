8a6ce0b12c044029713f123c3d71e102c21636ac
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2023-12-26 20:40:36 +0100

    rename

--- src/rvlib/menu/text_edit.rs
+++ src/rvlib/menu/text_edit.rs
@@ -3,9 +3,9 @@ use egui::{Response, Ui};
 pub fn text_edit(
     text: &mut String,
     are_tools_active: &mut bool,
-    mut f: impl FnMut(&mut String) -> Response,
+    mut f_text_edit: impl FnMut(&mut String) -> Response,
 ) -> Response {
-    let filter_txt_field = f(text);
+    let filter_txt_field = f_text_edit(text);
     *are_tools_active = if filter_txt_field.gained_focus() {
         false
     } else if filter_txt_field.lost_focus() {
