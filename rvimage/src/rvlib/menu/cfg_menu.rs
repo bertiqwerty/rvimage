da53df5fa7d508546f978d4c67ccfb69202946ad
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2024-09-28 22:55:17 +0200

    separate cfg menu

--- rvimage/src/rvlib/menu/cfg_menu.rs
+++ rvimage/src/rvlib/menu/cfg_menu.rs
@@ -1,14 +1,169 @@
-use std::fs::{self, File};
+use std::{
+    fs::{self, File},
+    path::Path,
+};
 
 use egui::{Area, Frame, Id, Order, Response, RichText, Ui, Visuals, Widget};
 use rvimage_domain::to_rv;
 
 use crate::{
-    cfg::{self, get_cfg_tmppath, write_cfg_str, Cache, Cfg, Connection},
+    cfg::{self, get_cfg_tmppath, write_cfg_str, AzureBlobCfgPrj, Cache, Cfg, Connection},
     file_util::get_prj_name,
     menu::ui_util::text_edit_singleline,
     result::trace_ok_err,
 };
+
+fn azure_cfg_menu(
+    ui: &mut Ui,
+    azure_cfg: &mut AzureBlobCfgPrj,
+    curprjpath: Option<&Path>,
+    are_tools_active: &mut bool,
+) {
+    egui::Grid::new("azure-cfg-menu")
+        .num_columns(2)
+        .show(ui, |ui| {
+            ui.label("Connection str path");
+            text_edit_singleline(ui, &mut azure_cfg.connection_string_path, are_tools_active)
+                .on_hover_text(azure_cfg.connection_string_path.clone());
+            if ui.button("browse").clicked() {
+                let csf = rfd::FileDialog::new().pick_file();
+                if let Some(csf) = csf {
+                    let relpath = curprjpath
+                        .and_then(|cpp| csf.strip_prefix(cpp).map_err(to_rv).ok())
+                        .and_then(|rp| rp.to_str());
+                    if let Some(relpath) = relpath {
+                        azure_cfg.connection_string_path = relpath.to_string();
+                    } else if let Some(csf_s) = csf.to_str() {
+                        azure_cfg.connection_string_path = csf_s.to_string();
+                    }
+                }
+            }
+            ui.end_row();
+            ui.label("Blob container name");
+            text_edit_singleline(ui, &mut azure_cfg.container_name, are_tools_active)
+                .on_hover_text(azure_cfg.container_name.clone());
+            ui.end_row();
+            ui.label("Prefix/folder");
+            text_edit_singleline(ui, &mut azure_cfg.prefix, are_tools_active)
+                .on_hover_text(azure_cfg.prefix.clone());
+        });
+}
+
+enum Close {
+    Yes(bool),
+    No,
+}
+
+fn settings_popup(ui: &mut Ui, cfg: &mut Cfg, are_tools_active: &mut bool) -> Close {
+    let mut close = Close::No;
+    Frame::popup(ui.style()).show(ui, |ui| {
+        ui.horizontal(|ui| {
+            if ui.button("Open in Editor").clicked() {
+                // to show the current config in an external editor, we need to save it first
+                let tmppath = get_cfg_tmppath(cfg);
+                tmppath
+                    .parent()
+                    .and_then(|p| fs::create_dir_all(p).ok())
+                    .or_else(|| {
+                        tracing::error!("could not create directory for tmp file");
+                        Some(())
+                    });
+                trace_ok_err(File::create(&tmppath));
+                let log_tmp = false;
+                if let Err(e) =
+                    toml::to_string_pretty(&cfg).map(|s| write_cfg_str(&s, &tmppath, log_tmp))
+                {
+                    tracing::error!("could not write config,\n{e:#?}");
+                    tracing::error!("{:?}", cfg);
+                }
+                tracing::info!("opening {tmppath:?}");
+                if let Err(e) = edit::edit_file(&tmppath) {
+                    tracing::error!("{e:?}");
+                    tracing::error!("could not open editor. {:?}", edit::get_editor());
+                }
+                if let Some(cfg_) = trace_ok_err(cfg::read_cfg_gen::<Cfg>(&tmppath)) {
+                    *cfg = cfg_;
+                }
+                if let Err(e) = cfg::write_cfg(cfg) {
+                    tracing::error!("could not save cfg {e:?}");
+                }
+            }
+            if ui.button("OK").clicked() {
+                close = Close::Yes(true);
+            }
+            if ui.button("Cancel").clicked() {
+                close = Close::Yes(false);
+            }
+        });
+        ui.horizontal(|ui| {
+            let name = get_prj_name(cfg.current_prj_path(), None);
+            ui.label("Project Name");
+            ui.label(RichText::from(name).text_style(egui::TextStyle::Monospace));
+        });
+        ui.separator();
+        ui.horizontal(|ui| {
+            ui.label("Style");
+            if ui.visuals().dark_mode {
+                if ui.button("Light").clicked() {
+                    cfg.usr.darkmode = Some(false);
+                    ui.ctx().set_visuals(Visuals::light());
+                }
+            } else if ui.button("Dark").clicked() {
+                cfg.usr.darkmode = Some(true);
+                ui.ctx().set_visuals(Visuals::dark());
+            }
+        });
+        ui.separator();
+        ui.horizontal(|ui| {
+            let mut autosave = cfg.usr.n_autosaves.unwrap_or(0);
+            ui.label("Autosave versions");
+            ui.add(egui::Slider::new(&mut autosave, 0..=10));
+            if autosave > 0 {
+                cfg.usr.n_autosaves = Some(autosave);
+            } else {
+                cfg.usr.n_autosaves = None;
+            }
+        });
+        ui.separator();
+        ui.label("Connection");
+        ui.radio_value(&mut cfg.prj.connection, Connection::Local, "Local");
+        ui.radio_value(&mut cfg.prj.connection, Connection::Ssh, "Ssh");
+        ui.radio_value(
+            &mut cfg.prj.connection,
+            Connection::PyHttp,
+            "Http served by 'python -m http.server'",
+        );
+        #[cfg(feature = "azure_blob")]
+        ui.radio_value(
+            &mut cfg.prj.connection,
+            Connection::AzureBlob,
+            "Azure blob experimental",
+        );
+        if cfg.prj.connection == Connection::AzureBlob {
+            let curprjpath = cfg.current_prj_path().parent().map(|cpp| cpp.to_path_buf());
+            if let Some(azure_cfg) = &mut cfg.prj.azure_blob {
+                azure_cfg_menu(ui, azure_cfg, curprjpath.as_deref(), are_tools_active);
+            }
+        }
+        ui.separator();
+        ui.horizontal(|ui| {
+            ui.label("Cache");
+            ui.radio_value(&mut cfg.usr.cache, Cache::FileCache, "File Cache");
+            ui.radio_value(&mut cfg.usr.cache, Cache::NoCache, "No Cache");
+        });
+        ui.separator();
+        ui.separator();
+        ui.horizontal(|ui| {
+            if ui.button("OK").clicked() {
+                close = Close::Yes(true);
+            }
+            if ui.button("Cancel").clicked() {
+                close = Close::Yes(false);
+            }
+        })
+    });
+    close
+}
 
 pub struct CfgMenu<'a> {
     id: Id,
@@ -33,176 +188,15 @@ impl<'a> Widget for CfgMenu<'a> {
         if edit_cfg_btn_resp.clicked() {
             ui.memory_mut(|m| m.toggle_popup(self.id));
         }
-        enum Close {
-            Yes(bool),
-            No,
-        }
-        let mut close = Close::No;
         if ui.memory(|m| m.is_popup_open(self.id)) {
             let area = Area::new(self.id)
                 .order(Order::Foreground)
                 .default_pos(edit_cfg_btn_resp.rect.left_bottom());
 
+            let mut close = Close::No;
             let area_response = area
                 .show(ui.ctx(), |ui| {
-                    Frame::popup(ui.style()).show(ui, |ui| {
-                        ui.horizontal(|ui| {
-                            if ui.button("Open in Editor").clicked() {
-                                // to show the current config in an external editor, we need to save it first
-                                let tmppath = get_cfg_tmppath(self.cfg);
-                                tmppath
-                                    .parent()
-                                    .and_then(|p| fs::create_dir_all(p).ok())
-                                    .or_else(|| {
-                                        tracing::error!("could not create directory for tmp file");
-                                        Some(())
-                                    });
-                                trace_ok_err(File::create(&tmppath));
-                                let log_tmp = false;
-                                if let Err(e) = toml::to_string_pretty(&self.cfg)
-                                    .map(|s| write_cfg_str(&s, &tmppath, log_tmp))
-                                {
-                                    tracing::error!("could not write config,\n{e:#?}");
-                                    tracing::error!("{:?}", self.cfg);
-                                }
-                                tracing::info!("opening {tmppath:?}");
-                                if let Err(e) = edit::edit_file(&tmppath) {
-                                    tracing::error!("{e:?}");
-                                    tracing::error!(
-                                        "could not open editor. {:?}",
-                                        edit::get_editor()
-                                    );
-                                }
-                                if let Some(cfg) = trace_ok_err(cfg::read_cfg_gen::<Cfg>(&tmppath))
-                                {
-                                    *self.cfg = cfg;
-                                }
-                                if let Err(e) = cfg::write_cfg(self.cfg) {
-                                    tracing::error!("could not save cfg {e:?}");
-                                }
-                            }
-                            if ui.button("OK").clicked() {
-                                close = Close::Yes(true);
-                            }
-                            if ui.button("Cancel").clicked() {
-                                close = Close::Yes(false);
-                            }
-                        });
-                        ui.horizontal(|ui| {
-                            let name = get_prj_name(self.cfg.current_prj_path(), None);
-                            ui.label("Project Name");
-                            ui.label(RichText::from(name).text_style(egui::TextStyle::Monospace));
-                        });
-                        ui.separator();
-                        ui.horizontal(|ui| {
-                            ui.label("Style");
-                            if ui.visuals().dark_mode {
-                                if ui.button("Light").clicked() {
-                                    self.cfg.usr.darkmode = Some(false);
-                                    ui.ctx().set_visuals(Visuals::light());
-                                }
-                            } else if ui.button("Dark").clicked() {
-                                self.cfg.usr.darkmode = Some(true);
-                                ui.ctx().set_visuals(Visuals::dark());
-                            }
-                        });
-                        ui.separator();
-                        ui.horizontal(|ui| {
-                            let mut autosave = self.cfg.usr.n_autosaves.unwrap_or(0);
-                            ui.label("Autosave versions");
-                            ui.add(egui::Slider::new(&mut autosave, 0..=10));
-                            if autosave > 0 {
-                                self.cfg.usr.n_autosaves = Some(autosave);
-                            } else {
-                                self.cfg.usr.n_autosaves = None;
-                            }
-                        });
-                        ui.separator();
-                        ui.label("Connection");
-                        ui.radio_value(&mut self.cfg.prj.connection, Connection::Local, "Local");
-                        ui.radio_value(&mut self.cfg.prj.connection, Connection::Ssh, "Ssh");
-                        ui.radio_value(
-                            &mut self.cfg.prj.connection,
-                            Connection::PyHttp,
-                            "Http served by 'python -m http.server'",
-                        );
-                        #[cfg(feature = "azure_blob")]
-                        ui.radio_value(
-                            &mut self.cfg.prj.connection,
-                            Connection::AzureBlob,
-                            "Azure blob experimental",
-                        );
-                        if self.cfg.prj.connection == Connection::AzureBlob {
-                            let curprjpath = self
-                                .cfg
-                                .current_prj_path()
-                                .parent()
-                                .map(|cpp| cpp.to_path_buf());
-                            if let Some(azure_cfg) = &mut self.cfg.prj.azure_blob {
-                                egui::Grid::new("azure-cfg-menu")
-                                    .num_columns(2)
-                                    .show(ui, |ui| {
-                                        ui.label("Connection str path");
-                                        text_edit_singleline(
-                                            ui,
-                                            &mut azure_cfg.connection_string_path,
-                                            self.are_tools_active,
-                                        )
-                                        .on_hover_text(azure_cfg.connection_string_path.clone());
-                                        if ui.button("browse").clicked() {
-                                            let csf = rfd::FileDialog::new().pick_file();
-                                            if let Some(csf) = csf {
-                                                let relpath = curprjpath
-                                                    .and_then(|cpp| {
-                                                        csf.strip_prefix(cpp).map_err(to_rv).ok()
-                                                    })
-                                                    .and_then(|rp| rp.to_str());
-                                                if let Some(relpath) = relpath {
-                                                    azure_cfg.connection_string_path =
-                                                        relpath.to_string();
-                                                } else if let Some(csf_s) = csf.to_str() {
-                                                    azure_cfg.connection_string_path =
-                                                        csf_s.to_string();
-                                                }
-                                            }
-                                        }
-                                        ui.end_row();
-
-                                        ui.label("Blob container name");
-                                        text_edit_singleline(
-                                            ui,
-                                            &mut azure_cfg.container_name,
-                                            self.are_tools_active,
-                                        )
-                                        .on_hover_text(azure_cfg.container_name.clone());
-                                        ui.end_row();
-                                        ui.label("Prefix/folder");
-                                        text_edit_singleline(
-                                            ui,
-                                            &mut azure_cfg.prefix,
-                                            self.are_tools_active,
-                                        )
-                                        .on_hover_text(azure_cfg.prefix.clone());
-                                    });
-                            }
-                        }
-                        ui.separator();
-                        ui.horizontal(|ui| {
-                            ui.label("Cache");
-                            ui.radio_value(&mut self.cfg.usr.cache, Cache::FileCache, "File Cache");
-                            ui.radio_value(&mut self.cfg.usr.cache, Cache::NoCache, "No Cache");
-                        });
-                        ui.separator();
-                        ui.separator();
-                        ui.horizontal(|ui| {
-                            if ui.button("OK").clicked() {
-                                close = Close::Yes(true);
-                            }
-                            if ui.button("Cancel").clicked() {
-                                close = Close::Yes(false);
-                            }
-                        })
-                    });
+                    close = settings_popup(ui, self.cfg, self.are_tools_active);
                 })
                 .response;
             if let Close::Yes(save) = close {
