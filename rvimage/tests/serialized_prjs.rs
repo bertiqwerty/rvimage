18630679bdf6683274958fd7cdae6319fc97d871
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2024-12-14 15:45:50 +0100

    project import test for importing self

--- rvimage/tests/serialized_prjs.rs
+++ rvimage/tests/serialized_prjs.rs
@@ -57,13 +57,13 @@ fn test_prj_v4_0() {
 
 #[test]
 fn test_import() {
-    fn test(src1: PathBuf, src2: PathBuf, reference: PathBuf) {
+    fn test(src1: &Path, src2: &Path, reference: &Path) {
         let mut ctrl = get_ctrl();
         let src1 = tmp_copy(&src1);
         defer_file_removal!(&src1);
         let mut tdm_merged = ctrl.load(src1.clone()).unwrap();
         thread::sleep(Duration::from_millis(5));
-        ctrl.import(src2, &mut tdm_merged).unwrap();
+        ctrl.import(src2.to_path_buf(), &mut tdm_merged).unwrap();
         thread::sleep(Duration::from_millis(5));
 
         let reference = tmp_copy(&reference);
@@ -88,11 +88,10 @@ fn test_import() {
                 for k in merged_map.keys() {
                     let (merged_annos, merged_shape) = merged_map.get(k).unwrap();
                     let (ref_annos, ref_shape) = ref_map.get(k).unwrap();
+                    assert_eq!(merged_annos.len(), ref_annos.len());
+                    assert!(merged_annos.len() > 0);
                     for elt in merged_annos.elts() {
                         assert!(ref_annos.elts().contains(elt));
-                    }
-                    for elt in ref_annos.elts() {
-                        assert!(merged_annos.elts().contains(elt));
                     }
                     assert_eq!(merged_annos, ref_annos);
                     assert_eq!(merged_shape, ref_shape);
@@ -118,5 +117,6 @@ fn test_import() {
     let test_file_src_2 =
         get_test_folder().join(get_test_folder().join("import-test-src-treelabel.json"));
     let test_file_src_ref = get_test_folder().join(get_test_folder().join("import-test.json"));
-    test(test_file_src_1, test_file_src_2, test_file_src_ref);
+    test(&test_file_src_1, &test_file_src_2, &test_file_src_ref);
+    test(&test_file_src_ref, &test_file_src_ref, &test_file_src_ref);
 }
