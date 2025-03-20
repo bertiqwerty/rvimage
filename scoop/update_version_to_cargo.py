d76c089adb74c87df15c581da279f25fb3c0c4a2
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2025-03-20 10:46:59 +0100

    add scoop manifests

---
+++ scoop/update_version_to_cargo.py
@@ -0,0 +1,43 @@
+import json
+from pathlib import Path
+import tomllib
+import urllib.request
+
+
+def main():
+    manifest = "scoop/bucket/rvimage.json"
+    with open(manifest, "r") as f:
+        scoop_data = json.load(f)
+
+    with open("rvimage/Cargo.toml", "rb") as f:
+        cargo_data = tomllib.load(f)
+    cargo_version = cargo_data["package"]["version"]
+    old_version = scoop_data["version"]
+    new_version = f"v{cargo_version}"
+    if old_version == new_version:
+        print("No version change")
+        return
+    manifest_p = Path(manifest)
+    manifest_old = manifest_p.rename(manifest_p.with_name(f"rvimage-{old_version}.json"))
+    with open(manifest_old, "w") as f:
+        json.dump(scoop_data, f, indent=4)
+    scoop_data["version"] = new_version
+    url = scoop_data["architecture"]["64bit"]["url"]
+    scoop_data["architecture"]["64bit"]["url"] = url.replace(old_version, new_version)
+    scoop_data["extract_dir"] = scoop_data["extract_dir"].replace(
+        old_version, new_version
+    )
+    checksum_file, _ = urllib.request.urlretrieve(
+        "https://github.com/bertiqwerty/rvimage/releases/download/"
+        f"{new_version}/rvimage-{new_version}-x86_64-pc-windows-msvc.zip.sha256",
+    )
+    with open(checksum_file, "r") as f:
+        checksum = f.readlines()[1].strip()
+    scoop_data["architecture"]["64bit"]["hash"] = checksum
+    print(scoop_data)
+    with open(manifest, "w") as f:
+        json.dump(scoop_data, f, indent=4)
+
+
+if __name__ == "__main__":
+    main()
