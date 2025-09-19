f29357f56c07b5306279544d855ca4df27698f5d
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2025-09-19 09:53:21 +0200

    add tag push

--- rvimage-py/publish.py
+++ rvimage-py/publish.py
@@ -8,9 +8,10 @@ def main():
         pyprj = tomllib.load(f)
     version = pyprj["project"]["version"]
     print(f"{version}")
-    os.system(f'git tag -a pypi-pub-v{version} -m "publish version {version}"')
     os.system("uv build")
     os.system("uv publish")
+    os.system(f'git tag -a pypi-pub-v{version} -m "publish version {version}"')
+    os.system(f"git push origin pypi-pub-v{version}")
 
 
 if __name__ == "__main__":
